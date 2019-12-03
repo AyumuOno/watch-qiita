1b052a08e079a09497171ed83ea83f6b0fabce9a
小野歩 <onoayumu@kut192-168-11-004.jim-dmz.kochi-tech.ac.jp>
2019-12-03 10:10:57 +0900

    サンプルとしてlivedoorのdescriptionデータをボタンを押すことで表示するようにした

---
+++ app/src/main/java/com/example/watchqiita/HttpUtil.kt
@@ -0,0 +1,18 @@
+package com.example.watchqiita
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
+class HttpUtil {
+    fun httpGET1(url : String): String? {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        val response = client.newCall(request).execute()
+        val body = response.body?.string()
+        return body
+    }
+}
---
+++ app/src/main/java/com/example/watchqiita/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.watchqiita
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.eclipsesource.json.Json
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    val URL =
+        "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040" //サンプルとしてライブドアのお天気Webサービスを利用します（40040は久米市のID）
+    var result = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val getButton = findViewById(R.id.button) as Button
+        getButton.setOnClickListener(object : View.OnClickListener {
+            override
+            fun onClick(view: View) {
+                onParallelGetButtonClick()
+            }
+        })
+    }
+
+    //非同期処理でHTTP GETを実行します。
+    fun onParallelGetButtonClick() = GlobalScope.launch(Dispatchers.Main) {
+        val http = HttpUtil()
+        //Mainスレッドでネットワーク関連処理を実行するとエラーになるためBackgroundで実行
+        async(Dispatchers.Default) { http.httpGET1(URL) }.await().let {
+            //minimal-jsonを使って　jsonをパース
+            val result = Json.parse(it).asObject()
+            val textView = findViewById(R.id.text) as TextView
+            textView.setText(result.get("description").asObject().get("text").asString())
+        }
+    }
+}
